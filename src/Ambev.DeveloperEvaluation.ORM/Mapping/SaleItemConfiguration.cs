8a3223c8dbc5bb9ba60e50da33fce43be2765dfd
Henrique Andrade <henrique.andradesilva@hotmail.com>
2025-01-23 14:49:14 -0300

    feat: create SaleConfiguration, SaleItemConfiguration, and update UserConfiguration

---
+++ src/Ambev.DeveloperEvaluation.ORM/Mapping/SaleItemConfiguration.cs
@@ -0,0 +1,46 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Ambev.DeveloperEvaluation.ORM.Mapping;
+
+public class SaleItemConfiguration : IEntityTypeConfiguration<SaleItem>
+{
+    public void Configure(
+        EntityTypeBuilder<SaleItem> builder)
+    {
+        builder.ToTable("SaleItems");
+
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Id)
+               .HasColumnType("uuid")
+               .HasDefaultValueSql("gen_random_uuid()");
+
+        builder.Property(x => x.Product)
+               .IsRequired()
+               .HasMaxLength(150);
+
+        builder.Property(x => x.Quantity)
+               .IsRequired();
+
+        builder.Property(x => x.UnitPrice)
+               .IsRequired()
+               .HasPrecision(18, 2);
+
+        builder.Property(x => x.Discount)
+               .IsRequired()
+               .HasPrecision(18, 2);
+
+        builder.Property(x => x.TotalAmount)
+               .IsRequired()
+               .HasPrecision(18, 2);
+
+        builder.Property(x => x.IsCancelled)
+            .IsRequired();
+
+        builder.HasOne<Sale>()
+               .WithMany(s => s.ListSaleItems)
+               .HasForeignKey(x => x.SaleId)
+               .OnDelete(DeleteBehavior.Cascade);
+    }
+}
