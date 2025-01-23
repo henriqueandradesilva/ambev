f3a6bfe19001bc5c28bded1240556ea4e8d8f790
Henrique Andrade <henrique.andradesilva@hotmail.com>
2025-01-23 14:48:32 -0300

    feat: add ORM Repository, SaleItemRepository, SaleRepository, and update UserRepository

---
+++ src/Ambev.DeveloperEvaluation.ORM/Repositories/SaleItemRepository.cs
@@ -0,0 +1,19 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+using Ambev.DeveloperEvaluation.Domain.Repositories;
+
+namespace Ambev.DeveloperEvaluation.ORM.Repositories;
+
+/// <summary>
+/// Implementation of the <see cref="ISaleItemRepository"/> interface using Entity Framework Core.
+/// Provides methods to manage sale items in the database.
+/// </summary>
+public class SaleItemRepository : Repository<SaleItem>, ISaleItemRepository
+{
+    private readonly DefaultContext _context;
+
+    public SaleItemRepository(
+        DefaultContext context) : base(context)
+    {
+        _context = context;
+    }
+}
