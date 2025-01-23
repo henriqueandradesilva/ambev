4df8db515889ef50a3065c6280c0c80f51612c75
Henrique Andrade <henrique.andradesilva@hotmail.com>
2025-01-23 14:46:26 -0300

    feat: add IRepository interface and implement SaleItemRepository, SaleRepository, and update IUserRepository

---
+++ src/Ambev.DeveloperEvaluation.Domain/Repositories/ISaleItemRepository.cs
@@ -0,0 +1,10 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+
+namespace Ambev.DeveloperEvaluation.Domain.Repositories;
+
+/// <summary>
+/// Defines the contract for accessing and managing <see cref="SaleItem"/> entities in the repository.
+/// </summary>
+public interface ISaleItemRepository : IRepository<SaleItem>
+{
+}
---
+++ src/Ambev.DeveloperEvaluation.Domain/Repositories/ISaleRepository.cs
@@ -0,0 +1,16 @@
+﻿using Ambev.DeveloperEvaluation.Common.Utils;
+using Ambev.DeveloperEvaluation.Domain.Entities;
+
+namespace Ambev.DeveloperEvaluation.Domain.Repositories;
+
+/// <summary>
+/// Defines the contract for accessing and managing <see cref="Sale"/> entities in the repository.
+/// </summary>
+public interface ISaleRepository : IRepository<Sale>
+{
+    Task<PaginatedList<Sale>> GetListSaleWithPaginationAsync(
+        int pageNumber,
+        int pageSize,
+        Func<IQueryable<Sale>, IQueryable<Sale>>? queryCustomizer = null,
+        CancellationToken cancellationToken = default);
+}
