241ca93608c5060362ced837a91df972a75b8cf3
Henrique Andrade <henrique.andradesilva@hotmail.com>
2025-01-23 15:12:58 -0300

    feat: add CreateSaleRequestProfile

---
+++ src/Ambev.DeveloperEvaluation.WebApi/Mappings/CreateSaleRequestProfile.cs
@@ -0,0 +1,18 @@
+﻿using Ambev.DeveloperEvaluation.Application.Sales.Commands.CreateSale;
+using Ambev.DeveloperEvaluation.WebApi.Features.Sales.CreateSale;
+using Ambev.DeveloperEvaluation.WebApi.Features.Sales.GetSale;
+using AutoMapper;
+
+namespace Ambev.DeveloperEvaluation.WebApi.Mappings;
+
+public class CreateSaleRequestProfile : Profile
+{
+    public CreateSaleRequestProfile()
+    {
+        CreateMap<CreateSaleRequest, CreateSaleCommand>();
+        CreateMap<CreateSaleItemRequest, CreateSaleItemCommand>();
+
+        CreateMap<CreateSaleResult, CreateSaleResponse>();
+        CreateMap<CreateSaleItemResult, GetSaleItemResponse>();
+    }
+}
