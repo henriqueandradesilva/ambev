ca461e6ef20b96448ce448ddd59635f2f1b52e4c
Henrique Andrade <henrique.andradesilva@hotmail.com>
2025-01-23 15:11:05 -0300

    feat: add UpdateUser and GetListUser with pagination in WebAPI

---
+++ src/Ambev.DeveloperEvaluation.WebApi/Features/Users/GetListUser/GetListUserProfile.cs
@@ -0,0 +1,20 @@
+﻿using Ambev.DeveloperEvaluation.Application.Users.Queries.GetListUser;
+using Ambev.DeveloperEvaluation.Domain.Entities;
+using AutoMapper;
+
+namespace Ambev.DeveloperEvaluation.WebApi.Features.Users.GetListUser;
+
+/// <summary>
+/// Profile for AutoMapper configuration for Users related mappings.
+/// </summary>
+public class GetListUserProfile : Profile
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="GetListUserProfile"/> class.
+    /// </summary>
+    public GetListUserProfile()
+    {
+        CreateMap<User, GetListUserResult>();
+        CreateMap<GetListUserResult, GetListUserResponse>();
+    }
+}
---
+++ src/Ambev.DeveloperEvaluation.WebApi/Features/Users/UpdateUser/UpdateUserResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace Ambev.DeveloperEvaluation.WebApi.Features.Users.UpdateUser;
+
+/// <summary>
+/// API response model for UpdateUser operation
+/// </summary>
+public class UpdateUserResponse
+{
+    /// <summary>
+    /// The unique identifier of the updated user
+    /// </summary>
+    public Guid Id { get; set; }
+}
