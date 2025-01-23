40241f671b1de818e73e972dfeb4fa17ad1ef837
Henrique Andrade <henrique.andradesilva@hotmail.com>
2025-01-23 15:03:49 -0300

    chore: organize IoC interfaces into dedicated folder

---
+++ src/Ambev.DeveloperEvaluation.IoC/ModuleInitializers/Interfaces/IModuleInitializer.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace Ambev.DeveloperEvaluation.IoC.ModuleInitializers.Interfaces;
+
+public interface IModuleInitializer
+{
+    void Initialize(
+        WebApplicationBuilder builder);
+}
