26c6ed4a6268ccab7a05658cb3db846ea340b7fe
Henrique Andrade <henrique.andradesilva@hotmail.com>
2025-01-23 15:09:15 -0300

    feat: create class to apply migrations automatically

---
+++ src/Ambev.DeveloperEvaluation.WebApi/Extensions/MigrationExtension.cs
@@ -0,0 +1,21 @@
+﻿using Ambev.DeveloperEvaluation.ORM;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ambev.DeveloperEvaluation.WebApi.Extensions;
+
+public static class MigrationExtension
+{
+    public static void ApplyMigrations(
+        this IApplicationBuilder app)
+    {
+        using IServiceScope scope = app.ApplicationServices.CreateScope();
+
+        var context = scope.ServiceProvider.GetRequiredService<DefaultContext>();
+
+        var connection =
+            context.Database.GetConnectionString();
+
+        if (context.Database.GetPendingMigrations().Any())
+            context.Database.Migrate();
+    }
+}
