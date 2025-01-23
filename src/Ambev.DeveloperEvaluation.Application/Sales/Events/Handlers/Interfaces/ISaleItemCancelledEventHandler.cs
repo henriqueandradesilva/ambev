a05a2eba96fc0e47320d76012d90c3231b0ba411
Henrique Andrade <henrique.andradesilva@hotmail.com>
2025-01-23 15:00:58 -0300

    feat: add events and interfaces for message broker integration

---
+++ src/Ambev.DeveloperEvaluation.Application/Sales/Events/Handlers/Interfaces/ISaleItemCancelledEventHandler.cs
@@ -0,0 +1,10 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Events.Sales;
+
+namespace Ambev.DeveloperEvaluation.Application.Sales.Events.Handlers.Interfaces;
+
+public interface ISaleItemCancelledEventHandler
+{
+    Task Handle(
+        SaleItemCancelledEvent notification,
+        CancellationToken cancellationToken);
+}
---
+++ src/Ambev.DeveloperEvaluation.Application/Sales/Events/Handlers/SaleModifiedEventHandler.cs
@@ -0,0 +1,36 @@
+﻿using Ambev.DeveloperEvaluation.Application.Sales.Events.Handlers.Interfaces;
+using Ambev.DeveloperEvaluation.Domain.Events.Sales;
+using Ambev.DeveloperEvaluation.MessageBroker.Interfaces;
+
+namespace Ambev.DeveloperEvaluation.Application.Sales.Events.Handlers;
+
+/// <summary>
+/// Handles the event when a sale is modified by publishing it to the event queue.
+/// </summary>
+public class SaleModifiedEventHandler : MessageBroker.EventHandler<SaleModifiedEvent>, ISaleModifiedEventHandler
+{
+    private readonly IEventPublisher _eventPublisher;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="SaleModifiedEventHandler"/> class.
+    /// </summary>
+    /// <param name="eventPublisher">The event Publisher for publishing events to a message queue.</param>
+    public SaleModifiedEventHandler(
+        IEventPublisher eventPublisher) : base(eventPublisher)
+    {
+        _eventPublisher = eventPublisher;
+    }
+
+    /// <summary>
+    /// Handles the <see cref="SaleModifiedEvent"/> by publishing it to the appropriate message queue.
+    /// </summary>
+    /// <param name="notification">The sale modification event data.</param>
+    /// <param name="cancellationToken">A token to observe while waiting for the task to complete.</param>
+    /// <returns>A task representing the asynchronous operation.</returns>
+    public override async Task Handle(
+        SaleModifiedEvent notification,
+        CancellationToken cancellationToken)
+    {
+        await _eventPublisher.PublishToQueueAsync(notification, nameof(SaleModifiedEvent));
+    }
+}
